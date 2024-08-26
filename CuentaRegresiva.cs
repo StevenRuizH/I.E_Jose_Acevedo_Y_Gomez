c4fbe2dec49af1e9a4558fa443c9974e74e4dbd8
StevenRuizH <stevenruiz0207@gmail.com>
2024-08-26 16:49:25 -0500

    Actualizacion CuentaRegresiva.cs
    Me equivoque con la fecha :)

--- CuentaRegresiva.cs
+++ CuentaRegresiva.cs
@@ -2,7 +2,7 @@
 
 Autor:       Steven Ruiz - Juan Pablo Zuluaga -Sheyra Cuevas - Yeferson Nieto
 
-Fecha:       21-08-2024
+Fecha:       26-08-2024
 
 Descripcion: Cuenta regresiva del 99 al 1 usando el comando PARA.
 
