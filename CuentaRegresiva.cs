0d5b39022e1bdecc5d793a41a0a82ea3c02a142a
StevenRuizH <stevenruiz0207@gmail.com>
2024-08-26 16:10:21 -0500

    Creacion de CuentaRegresiva.cs
    Es un algoritmo para probar el comando PARA creando un programa para una cuenta regresiva del 99 al 1 usando el comando PARA.

---
+++ CuentaRegresiva.cs
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Autor:       Steven Ruiz - Juan Pablo Zuluaga -Sheyra Cuevas - Yeferson Nieto
+
+Fecha:       21-08-2024
+
+Descripcion: Cuenta regresiva del 99 al 1 usando el comando PARA.
+
+*******************************************************************************/
+using System; 
+
+class CuentaRegresiva
+{
+    static void Main()
+    {
+        int Num;
+        for (Num=99; Num>=1;Num=Num-1)
+        {
+            Console.WriteLine(Num);
+        }
+    }
+}
